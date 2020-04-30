4584b1b3787706132b0091efe5d4e9f9245aebc7
Heesu Yun <Yunheesu@icloud.com>
2020-05-01 00:52:59 +0900

    Part 1

--- Hackathon/ViewController.swift
+++ Hackathon/ViewController.swift
@@ -7,14 +7,116 @@
 //
 
 import UIKit
+import CoreLocation
+import Firebase
+import FirebaseUI
+import GoogleSignIn  // used to be called FirebaseGoogleAuthUI
 
 class ViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var teams = ["Boston College", "Georgia Tech", "Gonzaga University", "Smith College"]
+    // declaring the authUI variable is step [2]
+    var authUI: FUIAuth!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // initializing the authUI var and setting the delegate are step [3]
+        authUI = FUIAuth.defaultAuthUI()
+        authUI?.delegate = self
+        
+        // if you have a table view, you'll want this code, too. The .isHidden will hide the table view data so that it cannot be seen by someone who isn't logged in.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isHidden = true
+
+    }
+    // Be sure to call this from viewDidAppear
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        signIn()
     }
 
+    // VITAL: This gist includes key changes to make sure "cancel" works with iOS 13.
+    func signIn() { // from website
+        let providers: [FUIAuthProvider] = [
+            FUIGoogleAuth(),
+        ]
+        if authUI.auth?.currentUser == nil {
+            self.authUI?.providers = providers
+            let loginViewController = authUI.authViewController()
+            loginViewController.modalPresentationStyle = .fullScreen
+            present(loginViewController, animated: true, completion: nil)
+        } else {
+            tableView.isHidden = false
+        }
+    }
 
+    @IBAction func signOutPressed(_ sender: UIBarButtonItem) {
+        do { // from website
+            try authUI!.signOut()
+            print("^^^ Successfully signed out!")
+            tableView.isHidden = true
+            signIn()
+        } catch {
+            tableView.isHidden = true
+            print("*** ERROR: Couldn't sign out")
+        }
+    }
+    
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return teams.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TeamCell", for: indexPath)
+        cell.textLabel?.text = teams[indexPath.row]
+        return cell
+    }
+    
+}
+// Name of the extension is likely the only thing that needs to change in new projects
+extension ViewController: FUIAuthDelegate {
+    func application(_ app: UIApplication, open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+        let sourceApplication = options[UIApplication.OpenURLOptionsKey.sourceApplication] as! String?
+        if FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication) ?? false {
+            return true
+        }
+        // other URL handling goes here.
+        return false
+    }
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
+        if let user = user {
+            // Assumes data will be isplayed in a tableView that was hidden until login was verified so unauthorized users can't see data.
+            tableView.isHidden = false
+            print("^^^ We signed in with the user \(user.email ?? "unknown e-mail")")
+        }
+    }
+    func authPickerViewController(forAuthUI authUI: FUIAuth) -> FUIAuthPickerViewController { //picture on log in page (got from website)
+        
+        // Create an instance of the FirebaseAuth login view controller
+        let loginViewController = FUIAuthPickerViewController(authUI: authUI)
+        
+        // Set background color to white
+        loginViewController.view.backgroundColor = UIColor.white
+        
+        // Create a frame for a UIImageView to hold our logo
+        let marginInsets: CGFloat = 16 // logo will be 16 points from L and R margins
+        let imageHeight: CGFloat = 225 // the height of our logo
+        let imageY = self.view.center.y - imageHeight // places bottom of UIImageView in the center of the login screen
+        let logoFrame = CGRect(x: self.view.frame.origin.x + marginInsets, y: imageY, width: self.view.frame.width - (marginInsets*2), height: imageHeight)
+        
+        // Create the UIImageView using the frame created above & add the "logo" image
+        let logoImageView = UIImageView(frame: logoFrame)
+        logoImageView.image = UIImage(named: "logo")
+        logoImageView.contentMode = .scaleAspectFit // Set imageView to Aspect Fit
+        loginViewController.view.addSubview(logoImageView) // Add ImageView to the login controller's main view
+        return loginViewController
+    }
+}
